c3578d0930cf283016aa6c8d90d33714d7548572
SHubinaOlga <olga.shubina85@yandex.ru>
2024-09-17 20:56:26 +0300

    main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ class House:  # ('ЖК Эльбрус', 30)
         self.name = name
         self.number_of_floors = int(number_of_floors)
         self.value = number_of_floors
-       
+      
     def go_to(self, new_floor: int):
         self.new_floor = new_floor
         if self.new_floor > self.number_of_floors or self.new_floor < 1:
@@ -20,25 +20,35 @@ class House:  # ('ЖК Эльбрус', 30)
         return (f'"Название: {self.name}, кол-во этажей: {self.number_of_floors}"')
 
     def __eq__(self, other):
-        return self.number_of_floors == other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors == other
 
     def __lt__(self, other):
-        return self.number_of_floors < other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors < other
+        return self.__eq__(other) or self.__lt__(other)
 
     def __le__(self, other):
-        return self.number_of_floors <= other.number_of_floors
+        return self.__eq__(other) or self.__lt__(other)
 
     def __gt__(self, other):
-        return self.number_of_floors > other.number_of_floors
+        return not self.__le__(other)
 
     def __ge__(self, other):
-        return self.number_of_floors >= self.number_of_floors
+        return not self.__lt__(other)
 
     def __ne__(self, other):
-        return self.number_of_floors != other.number_of_floors
+        return not self.__eq__(other)
 
     def __add__(self, value):
-        self.number_of_floors = self.number_of_floors + value
+        if isinstance(value, int):
+            self.number_of_floors += value
+        elif isinstance(value, House):
+            self.number_of_floors += value.number_of_floors
         return self
 
     def __radd__(self, value):
