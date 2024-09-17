aef6ead87cc91f0f1e9ff15eb0f2c606c19a90d6
SHubinaOlga <olga.shubina85@yandex.ru>
2024-09-17 22:20:57 +0300

    main.py

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ class House:  # ('ЖК Эльбрус', 30)
         self.name = name
         self.number_of_floors = int(number_of_floors)
         self.value = number_of_floors
-      
-    def go_to(self, new_floor: int):
+
+
+    def go_to(self, new_floor):
         self.new_floor = new_floor
         if self.new_floor > self.number_of_floors or self.new_floor < 1:
             print('"Такого этажа не существует"')
